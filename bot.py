0bc0a54a70da038b24e88566bca5a620c0ba60a0
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2025-03-21 03:01:19 +0000

    a

--- bot.py
+++ bot.py
@@ -1,33 +1,23 @@
-import logging
-from telegram.ext import Updater, CommandHandler, Bot
+from telegram.ext import Updater, CommandHandler
 import os
 
-# Telegram
 BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-bot = Bot(BOT_TOKEN)
 
 def start(update, context):
-    bot.send_message(chat_id=update.effective_chat.id, text="Hey hot stuff! I'm your digital bae. What's cookin'?")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hey hot stuff! I'm your digital bae. What's cookin'?")
 
-def help_command(update, context):
-    bot.send_message(chat_id=update.effective_chat.id, text="Help!")
+def help(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Help!")
 
 def main():
     updater = Updater(token=BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("help", help))
 
-    # Start the Bot
     updater.start_polling()
-
-    logging.info("Bot fucking started!")
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
     updater.idle()
-
-
-if __name__ == "__main__":
+ 
+if __name__ == '__main__':
     main()
