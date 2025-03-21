52d2c5942907a5111616bfde2164c3e2c33d02f1
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2025-03-21 02:06:01 +0000

    z

--- bot.py
+++ bot.py
@@ -3,37 +3,30 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 
 # Telegram
-BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+BOT_TOKEN = os.environ("TELEGRAM_BOT_TOKEN")
 
 def start(update):
     # user = add_user(update.effective_user)
     update.message.reply_text("I'm alive motherfucker! What you want?")
 
-
 def help_command(update):
-    """Send a message when the command /help is issued."""
     update.message.reply_text("Help!")
 
 
 def echo(update):
-    """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater("BOT_TOKEN")
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     # Start the Bot
     updater.start_polling()
